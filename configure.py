6a108fd505fc69819e2cb215d0d9f99cdaf586c5
Kevin Conte <kevin@conte.net>
2022-01-23 21:22:26 -0800

    added configure.py and template for all.nim

---
+++ configure.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+from io import TextIOWrapper
+from os import listdir
+import re
+
+def writeline(f: TextIOWrapper, msg: str = ""):
+  f.write(msg + "\n")
+
+
+regex = re.compile(r"^p\d{3}\.nim$")
+solvers = sorted(list(filter(regex.match, listdir("solvers"))))
+
+print("Creating all.nim . . .")
+with open("all.template") as f:
+  template = f.read().strip().split("\n")
+
+with open("all.nim", "w") as f:
+  for line in template:
+    if line != "{{ PROBLEM_IMPORT }}":
+      writeline(f, line)
+    else:
+      for solver in solvers:
+        solver = solver.replace(".nim", "")
+        writeline(f, f"import ./solvers/{solver}")
+
+print("Done.")
+
+print("Creating Makefile . . .")
+with open("Makefile", "w") as f:
+  writeline(f, "NIM_CFLAGS = -d:release --warning:UnusedImport:off")
+  writeline(f)
+
+  writeline(f, "all: bin/all")
+  writeline(f, "bin/all: all.nim")
+  writeline(f, "\tnim c $(NIM_CFLAGS) -o:$@ $^")
+  writeline(f)
+
+  for solver in solvers:
+    problemID = int(solver[1:-4])
+
+    writeline(f, f".PHONY: {problemID}")
+    writeline(f, f"{problemID}: bin/{problemID}")
+    writeline(f, f"bin/{problemID}: solvers/{solver}")
+    writeline(f, "\tnim c $(NIM_CFLAGS) -o:$@ $<")
+    writeline(f)
+
+  writeline(f, "clean:")
+  writeline(f, "\t$(RM) bin/*")
+  writeline(f, ".PHONY: clean all")
+
+  pass
+
+print("Done.")
